1fbdc3077889c0604b4381a555bdac19c77efef3
Lucas Vieira Brito <vieiralucas2205@gmail.com>
2022-09-01 22:06:31 -0300

    Melhorando o JavaScript

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,34 @@
-let name = document.querySelector("#name");
-let phone = document.querySelector("#phone");
-let id = document.querySelector("#id");
+let name_contact = document.getElementById("name");
+let phone_contact = document.getElementById("phone");
+let id_contact = document.getElementById("id");
+let contacts_area = document.getElementById("contacts-area");
+let arrayContacts = [];
+
+function printConstactArea(arrayContacts) {
+  contacts_area += arrayContacts;
+}
+
+function resetFields() {
+  name_contact.value = "";
+  phone_contact.value = "";
+  id_contact.value = "";
+}
+
+function addContact() {
+  if (
+    name_contact.value == "" ||
+    phone_contact.value == "" ||
+    id_contact.value == ""
+  ) {
+    alert("Preencha todos os campos!");
+  } else {
+    let contacts = {
+      name: name_contact.value,
+      phone: phone_contact.value,
+      id: id_contact.value,
+    };
+    arrayContacts.push(contacts);
+    resetFields();
+    printConstactArea(arrayContacts);
+  }
+}
